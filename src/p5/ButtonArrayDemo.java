ea0def79f0ce6c4f6fb34fdcf990fcde27f8f2f6
emailman <emailman@dtcc.edu>
2015-11-01 20:49:52 -0500

    Started the common button handler

--- src/p5/ButtonArrayDemo.java
+++ src/p5/ButtonArrayDemo.java
@@ -1,6 +1,8 @@
 package p5;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,10 +15,17 @@ import javafx.stage.Stage;
  */
 public class ButtonArrayDemo extends Application{
 
+    // Create an array of buttons
+    int numOfButtons = 10;
+    Button[] buttonsArray = new Button[numOfButtons];
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Button Array Demo");
+        // Get the pane for the scene
         primaryStage.setScene(new Scene(getPane(), 60, 400));
+
+        // Setup the stage
+        primaryStage.setTitle("Button Array Demo");
         primaryStage.setResizable(false);
         primaryStage.show();
     }
@@ -24,13 +33,26 @@ public class ButtonArrayDemo extends Application{
     protected Pane getPane() {
         Pane pane = new VBox(10);
         pane.setPadding(new Insets(40));
-        int numOfButtons = 10;
-        Button[] buttonsArray = new Button[numOfButtons];
+
         for (int i = numOfButtons - 1; i >= 0; i--) {
+
+            // Set the button number as text for the button
             buttonsArray[i] = new Button(Integer.toString(i + 1));
             buttonsArray[i].setMinWidth(50);
+
+            // Add the button to the pane and set the handler
             pane.getChildren().add(buttonsArray[i]);
+            buttonsArray[i].setOnAction(ButtonHandler);
         }
         return pane;
     }
+
+    // Build an event handler for the buttons
+    EventHandler<ActionEvent> ButtonHandler = e -> {
+        Button b = (Button)e.getSource();
+
+        int i = Integer.parseInt(b.getText());
+        System.out.println(i);
+
+    };
 }
